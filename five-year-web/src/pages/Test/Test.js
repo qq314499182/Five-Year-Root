933de6c693301ff8515cd890327f1344adb8f325
zhaokep <zhaokep@yonyou.com>
2018-12-10 22:36:55 +0800

    模拟数据,渲染页面

--- five-year-web/src/pages/Test/Test.js
+++ five-year-web/src/pages/Test/Test.js
@@ -4,27 +4,26 @@ import {Card} from 'antd';
 export default class TestFrom extends Component{
   constructor(props) {
     super(props);
-    this.state = {
-      cardList: [
-        {
-          id:1,
-          setup:'hello',
-          punchline:'world'
-        },
-        {
-          id:1,
-          setup:'hello',
-          punchline:'world'
-        }
-      ]
-    }
+    this.state = {data:[]};
+  }
+
+  componentDidMount(){
+    fetch('./mock/order.json').then(res=>{
+      if(res.ok){
+        res.json().then(data=>{
+          this.setState({
+            data
+          })
+        })
+      }
+    })
   }
 
   render() {
     return (
       <div>
         {
-          this.state.cardList.map(card => {
+          this.state.data.map(card => {
             return (
               <Card key={card.id}>
                 <div>Q: {card.setup}</div>
