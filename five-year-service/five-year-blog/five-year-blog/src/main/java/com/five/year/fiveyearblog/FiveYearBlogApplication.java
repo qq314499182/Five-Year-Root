a065c4e90c1f466998452b043923d032372e2423
zhaokep <zhaokep@yonyou.com>
2018-12-24 16:08:37 +0800

    博客后端服务初始化

---
+++ five-year-service/five-year-blog/five-year-blog/src/main/java/com/five/year/fiveyearblog/FiveYearBlogApplication.java
@@ -0,0 +1,14 @@
+package com.five.year.fiveyearblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveYearBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveYearBlogApplication.class, args);
+	}
+
+}
+
