4a1de950d21426d4a7ea5ed85912e655eed616d9
zhaokep <zhaokep@yonyou.com>
2018-12-27 10:09:01 +0800

    五岁--博客后端-解决跨域

---
+++ five-year-service/five-year-blog/src/main/java/com/five/year/fiveyearblog/config/MyWebAppConfigurer.java
@@ -0,0 +1,22 @@
+package com.five.year.fiveyearblog.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * @Description  解决跨域访问
+ * @Author zhaoke <zhaokep@yonyou.com>
+ * @Version V1.0.0
+ * @Date 2018/12/11
+ */
+@Configuration
+public class MyWebAppConfigurer extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+
+}
